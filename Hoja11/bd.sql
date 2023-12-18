9bcac633f9e9fcb9ebdb638efeec596050774449
Aníbal Páez Gallego <anibalpaezgallego@gmail.com>
2023-12-18 12:29:40 +0100

    Update Hoja 11

--- Hoja11/bd.sql
+++ Hoja11/bd.sql
@@ -47,9 +47,9 @@ create table noticias(
 use inmobiliaria;
 insert into noticias (Titulo, Texto, Fecha) values ('Nueva promocion en Nervion', '145 viviendas de lujo en urbanizacion ajardinada situadas en un entorno privilegiado', '2007-02-04');
 insert into noticias (Titulo, Texto, Categoria, Fecha) values ('Ultimas viviendas junto al rio', 'Apartamentos de 1 y 2 dormitorios, con fantasticas vistas. Excelentes condiciones de financiacion', 'Ofertas','2007-02-05');
-insert into noticias (Titulo, Texto, Categoria, Fecha, Imagen) values ('Apartamentos en el Puerto de Sta Maria', 'En la playa de Valdegrana, en primera linea de playa. Pisos reformados y completamente amueblados', 'Costas', '2007-02-06', 'apartamento8.jpg');
+insert into noticias (Titulo, Texto, Categoria, Fecha, Imagen) values ('Apartamentos en el Puerto de Sta Maria', 'En la playa de Valdegrana, en primera linea de playa. Pisos reformados y completamente amueblados', 'Costas', '2007-02-06', 'imagenes/apartamento8.jpg');
 insert into noticias (Titulo, Texto, Fecha) values ('Casa reformada en el barrio de la Juderia', 'Dos plantas y atico, 5 habitaciones patio interior, amplio garaje. Situada en una calle tranquila y a un paso del centro historico', '2004-02-07');
-insert into noticias (Titulo, Texto, Categoria, Fecha, Imagen) values ('Promocion en Costa Ballena', 'Con vistas al campo de golf, magnificas calidades, entorno ajardinado con piscina y servicio de vigilancia', 'Costas', '2007-02-09', 'apartamento9.jpg');
+insert into noticias (Titulo, Texto, Categoria, Fecha, Imagen) values ('Promocion en Costa Ballena', 'Con vistas al campo de golf, magnificas calidades, entorno ajardinado con piscina y servicio de vigilancia', 'Costas', '2007-02-09', 'imagenes/apartamento9.jpg');
 
 CREATE TABLE votos(
     ID INT(3) unsigned NOT NULL auto_increment PRIMARY KEY,
