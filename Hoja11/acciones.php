4014efa14860a4697ebdcd9a67bc52d3acd07e62
Aníbal Páez Gallego <anibalpaezgallego@gmail.com>
2023-12-21 10:45:43 +0100

    update hoja 11

--- Hoja11/acciones.php
+++ Hoja11/acciones.php
@@ -7,30 +7,22 @@ session_start();
 $usuario = $_SESSION['usuario'];
 $valor = $_SESSION['permisos'];
 
-if ($valor == "1") {
-    echo "<h2>Bienvenido, " . $_SESSION['usuario'] . "!</h2>";
+echo "<h2>Bienvenido, " . $_SESSION['usuario'] . "!</h2>";
 
-    echo '<a href="consulta_noticias.php?id=1"><button class="button">Consultar Noticia 1</button></a>';
-    echo '<a href="consulta_noticias2.php?id=2"><button class="button">Consultar Noticia 2</button></a>';
-    echo '<a href="consulta_noticias3.php?id=3"><button class="button">Consultar Noticia 3</button></a>';
-    echo '<a href="encuesta.php?id=3"><button class="button">Votar en encuesta</button></a>';
-    echo '<a href="encuesta_resultados.php?id=3"><button class="button">Resultados encuesta</button></a>';
+echo '<a href="consulta_noticias.php?id=1"><button class="button">Consultar Noticia 1</button></a>';
+echo '<a href="consulta_noticias2.php?id=2"><button class="button">Consultar Noticia 2</button></a>';
+echo '<a href="consulta_noticias3.php?id=3"><button class="button">Consultar Noticia 3</button></a>';
+echo '<a href="encuesta.php?id=3"><button class="button">Votar en encuesta</button></a>';
+echo '<a href="encuesta_resultados.php?id=3"><button class="button">Resultados encuesta</button></a>';
+
+if ($valor == "1") {
+    
     echo '<a href="eliminar_noticias.php"><button class="button">Borrar Noticia</button></a>';
     echo '<a href="insertar_noticias.php"><button class="button">Insertar Noticia</button></a>';
     echo '<a href="borrar_usuarios.php"><button class="button">Borrar Usuarios</button></a>';
-    echo '<a href="cerrar_sesion.php"><button class="button">Cerrar sesion</button></a>';
-} else if ($valor == "0") {
-    echo "<h2>Bienvenido, " . $_SESSION['usuario'] . "!</h2>";
-
-    echo '<a href="consulta_noticias.php?id=1"><button class="button">Consultar Noticia 1</button></a>';
-    echo '<a href="consulta_noticias2.php?id=2"><button class="button">Consultar Noticia 2</button></a>';
-    echo '<a href="consulta_noticias3.php?id=3"><button class="button">Consultar Noticia 3</button></a>';
-    echo '<a href="encuesta.php?id=3"><button class="button">Votar en encuesta</button></a>';
-    echo '<a href="encuesta_resultados.php?id=3"><button class="button">Resultados encuesta</button></a>';
-    echo '<a href="cerrar_sesion.php"><button class="button">Cerrar sesion</button></a>';
-} else {
-    echo "Error: el valor de permisos es desconocido.";
 }
+
+echo '<a href="cerrar_sesion.php"><button class="button">Cerrar sesion</button></a>';
 ?>
 
 <!DOCTYPE html>
--- Hoja11/eliminar_noticias.php
+++ Hoja11/eliminar_noticias.php
@@ -1,6 +1,6 @@
 <?php
 include("conectar.php");
-include("estilos.html");
+include "estilos.html";
 
 session_start();
 
