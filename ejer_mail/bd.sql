7696753b2850ccb8236a2f8e6e30a08e360f5e72
Aníbal Páez Gallego <anibalpaezgallego@gmail.com>
2024-01-19 09:43:37 +0100

    ejercicio empezado

---
+++ ejer_mail/bd.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS postales;
+CREATE DATABASE IF NOT EXISTS postales CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE postales;
+
+CREATE TABLE informacion (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(30),
+    direccion VARCHAR(50),
+    CP INT,
+    email VARCHAR(100),
+    fecha_nac DATE,
+) ENGINE=InnoDB;
