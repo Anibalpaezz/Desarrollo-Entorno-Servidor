067e5e7a38315abfd4254c1d249ecf0320a59255
Aníbal Páez Gallego <anibalpaezgallego@gmail.com>
2024-01-20 13:06:13 +0100

    avanzando

--- ejer_mail/tipos.php
+++ ejer_mail/tipos.php
@@ -5,7 +5,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     include "conectar.php";
 
     try {
-
         $consulta = "SELECT email FROM informacion";
         $correos = $conexion->prepare($consulta);
         $correos->execute();
@@ -27,6 +26,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Correos</title>
     <style>
+        @import url(https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap);
+
+        * {
+            font-family: Archivo Black;
+            font-weight: 100;
+            text-align: center;
+            align-items: center;
+        }
+
+        body {
+            background-color: #cab991;
+            color: #333;
+            text-align: center;
+            align-items: center;
+            justify-content: center;
+            margin: 0;
+        }
+
+        h1 {
+            color: #141414;
+        }
+
         img {
             width: 100px;
             height: 65px;
@@ -48,75 +69,109 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             padding: 5px;
             border: 2px solid #ccc;
         }
+
+        table {
+            margin-top: 15px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+
+        select,
+        input[type="text"] {
+            width: 200px;
+            padding: 10px;
+            margin-top: 10px;
+        }
+
+        button {
+            background-color: #006400;
+            color: beige;
+            padding: 10px;
+            border: none;
+            margin-top: 10px;
+            cursor: pointer;
+        }
+
+        select {
+            width: 200px;
+        }
+
+        button:hover,
+        select:hover {
+            background-color: #003300;
+        }
+
+        label,
+        #destinatario {
+            display: inline-block;
+            cursor: pointer;
+            margin: 5px;
+            padding: 5px;
+            border: 2px solid #ccc;
+        }
+
+        #tabla {}
     </style>
 </head>
 
 <body>
-    <h1>Envio de postales</h1>
-    <?php
-        if ($tipo == 'navidad') {
-            
-        } else if ($tipo == 'vienna') {
-            
-        } else {
-            
-        }
-    ?>
-    <table>
-        <tr>
-            <td>
-                <input type="radio" name="opciones" id="opcion1">
-                <label for="opcion1">
-                    <img src="fotos/navidad/1.jpg" alt="">
-                </label>
-            </td>
-            <td>
-                <input type="radio" name="opciones" id="opcion2">
-                <label for="opcion2">
-                    <img src="fotos/navidad/2.jpg" alt="">
-                </label>
-            </td>
-            <td>
-                <input type="radio" name="opciones" id="opcion3">
-                <label for="opcion3">
-                    <img src="fotos/navidad/3.jpg" alt="">
-                </label>
-            </td>
-            <td>
-                <input type="radio" name="opciones" id="opcion4">
-                <label for="opcion4">
-                    <img src="fotos/navidad/4.jpg" alt="">
-                </label>
-            </td>
-            <td>
-                <input type="radio" name="opciones" id="opcion5">
-                <label for="opcion5">
-                    <img src="fotos/navidad/5.jpg" alt="">
-                </label>
-            </td>
-        </tr>
-    </table>
+    <h1>Envío de postales</h1>
+    <div id="tabla">
+        <table>
+            <tr>
+                <td>
+                    <input type="radio" name="opciones" id="opcion1">
+                    <label for="opcion1">
+                        <img src="fotos/<?php echo $tipo ?>/1.jpg" alt="">
+                    </label>
+                </td>
+                <td>
+                    <input type="radio" name="opciones" id="opcion2">
+                    <label for="opcion2">
+                        <img src="fotos/<?php echo $tipo ?>/2.jpg" alt="">
+                    </label>
+                </td>
+                <td>
+                    <input type="radio" name="opciones" id="opcion3">
+                    <label for="opcion3">
+                        <img src="fotos/<?php echo $tipo ?>/3.jpg" alt="">
+                    </label>
+                </td>
+                <td>
+                    <input type="radio" name="opciones" id="opcion4">
+                    <label for="opcion4">
+                        <img src="fotos/<?php echo $tipo ?>/4.jpg" alt="">
+                    </label>
+                </td>
+                <td>
+                    <input type="radio" name="opciones" id="opcion5">
+                    <label for="opcion5">
+                        <img src="fotos/<?php echo $tipo ?>/5.jpg" alt="">
+                    </label>
+                </td>
+            </tr>
+        </table>
+    </div>
+
 
     <form action="" method="post">
-        Destinatario: <select name="" id="">
-            <?php
-            if ($correos->rowCount() > 0) {
-                foreach ($resultado_correos as $row) {
-                    echo "<option value='" . htmlspecialchars($row["email"], ENT_QUOTES) . "'>" . htmlspecialchars($row["email"]) . "</option>";
+        <div id="select"> <label for="destinatario">Destinatario</label><select name="destinatario" id="destinatario">
+                <?php
+                if ($correos->rowCount() > 0) {
+                    foreach ($resultado_correos as $row) {
+                        echo "<option value='" . htmlspecialchars($row["email"], ENT_QUOTES) . "'>" . htmlspecialchars($row["email"]) . "</option>";
+                    }
+                } else {
+                    echo "<option value=''>No hay opciones disponibles</option>";
                 }
-            } else {
-                echo "<option value=''>No hay opciones disponibles</option>";
-            }
 
-            $conexion = null;
-            ?>
-        </select>
-
-        Mensaje: <input type="text" name="mensaje" id="mensaje">
-
-        Tema: <input type="text" name="tema" id="tema">
-
-        <button type="submit">Enviar</button>
+                $conexion = null;
+                ?>
+            </select></div>
+        <div id="mensaje">Mensaje: <input type="text" name="mensaje" id="mensaje"></div>
+        <div id="tema">Tema: <input type="text" name="tema" id="tema"></div>
+        <div id="boton"><button type="submit">Enviar</button></div>
     </form>
 
     <script></script>
