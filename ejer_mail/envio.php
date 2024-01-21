dfba491619920b38d789705cde3f1f265804247f
Aníbal Páez Gallego <anibalpaezgallego@gmail.com>
2024-01-21 22:52:04 +0100

    cambios

--- ejer_mail/envio.php
+++ ejer_mail/envio.php
@@ -1,6 +1,9 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombreFotoSeleccionada = $_POST['src_foto_seleccionada'];
     $destinatario = $_POST['destinatario'];
@@ -13,42 +16,66 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     /* echo $nombreFotoSeleccionada; */
     /* echo $mensaje; */
 
-    require 'PHPMailer-master/src/Exception.php';
-    require 'PHPMailer-master/src/PHPMailer.php';
-    require 'PHPMailer-master/src/SMTP.php';
 
-    
-    
+    require "/var/www/html/ejer_mail/PHPMailer-master/src/PHPMailer.php";
+    require "/var/www/html/ejer_mail/PHPMailer-master/src/SMTP.php";
+
+
+
     $smtpServidor = "localhost";
     $smtpUsuario = "anibal@troyan";
     $smtpClave = "nico1234";
     $smtpPuerto = 25;
-    
-    $mail = new PHPMailer(true);
-    
-    try {
+
+    $mail = new PHPMailer();
+
+    /* require('/var/www/html/github/servidorphp/PHPMailer-master/src/PHPMailer.php');
+    require('/var/www/html/github/servidorphp/PHPMailer-master/src/SMTP.php');
+    $mail = new PHPMailer();
+    $mail->isSMTP();
+    $mail->Mailer = "SMTP";
+    $mail->SMTPAutoTLS = true;
+    $mail->isHTML(true);
+    $mail->Port = 25;
+    $mail->Host = "localhost";
+    $mail->SMTPAuth = true;
+    $mail->Username = "jefe@nicolas.com";
+    $mail->Password = "jefe";
+    $mail->From = "anibal@troyan.com;
+    $mail->FromName = "jefe";
+    $mail->Timeout = 30; */
+
+    /* try { */
         $mail->isSMTP();
-        $mail->Host = $smtpServidor;
+        $mail->Mailer = "SMTP";
+        $mail->SMTPAutoTLS = true;
+        $mail->isHTML(true);
+        $mail->Port = 25;
+        $mail->Host = "localhost";
         $mail->SMTPAuth = true;
-        $mail->Username = $smtpUsuario;
-        $mail->Password = $smtpClave;
-        $mail->Port = $smtpPuerto;
-        $mail->SMTPSecure = 'tsl';
-    
-        $mail->setFrom('anibal@troyan.com', 'Anibal');
-        $mail->addAddress('seresep879@rentaen.com', 'Yo tambien');
+        $mail->Username = "anibal@troyan.com";
+        $mail->Password = "nico1234";
+        $mail->From = "anibal@troyan.com";
+        $mail->FromName = "yo";
+
+
+        $mail->addAddress("nico@troyan.com");
         $mail->Subject = 'Probando';
         $mail->Body = 'Hola';
-    
-        $mail->send();
-        echo 'Correo enviado correctamente.';
-    } catch (Exception $e) {
+        
+
+        
+        
+
+        if ($mail->send()) {
+            echo 'Correo enviado correctamente.';
+        } else {
+            echo 'Error al enviar el correo: ', $mail->ErrorInfo;
+        }
+        
+    /* } catch (Exception $e) {
         echo 'Error al enviar el correo: ', $mail->ErrorInfo;
-    }
-
-
-
-
+    } */
 } else {
     header('location: index.html');
 }
